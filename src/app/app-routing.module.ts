148f8ebdf9ff11de94bd4a2407f51c913a710e8f
subrata04 <subrata.influxiq@gmail.com>
2019-07-15 16:53:00 +0530

    subrata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from '../app/home/home.component'
+import {HomeComponent} from '../app/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlatformFeaturesComponent } from './platform-features/platform-features.component';
+import {OurteamComponent} from "./ourteam/ourteam.component";
+import {ContactusComponent} from "./contactus/contactus.component";
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'platform-features', component: PlatformFeaturesComponent},
+  { path: 'ourteam', component: OurteamComponent},
+  { path: 'contactus', component: ContactusComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,20 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import{ ReactiveFormsModule, FormsModule} from '@angular/forms'
+import{ ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { PlatformFeaturesComponent } from './platform-features/platform-features.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { ContactusComponent } from './contactus/contactus.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PlatformFeaturesComponent,
+    OurteamComponent,
+    ContactusComponent
   ],
   imports: [
     DemoMaterialModule,
